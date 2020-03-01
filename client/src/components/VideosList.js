37654d23df41c102c71231a9fbcc59afe1f428a8
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-03-01 02:04:56 -0500

    Implemented add and remove item (with random uuid), will connect to backend next through redux

---
+++ client/src/components/VideosList.js
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { v4 as uuidv4 } from "uuid";
+
+class VideosList extends Component {
+  state = {
+    items: [
+      { id: uuidv4(), name: "Eggs" },
+      { id: uuidv4(), name: "Milk" },
+      { id: uuidv4(), name: "Steak" },
+      { id: uuidv4(), name: "Water" }
+    ]
+  };
+
+  onDeleteClick = id => {
+    this.props.deleteItem(id);
+  };
+
+  render() {
+    const { items } = this.state;
+    return (
+      <Container>
+        <Button
+          color="dark"
+          style={{ marginBottom: "2rem" }}
+          onClick={() => {
+            const name = prompt("Enter Item");
+            if (name) {
+              this.setState(state => ({
+                items: [...state.items, { id: uuidv4(), name }]
+              }));
+            }
+          }}
+        >
+          Add Item
+        </Button>
+
+        <ListGroup>
+          <TransitionGroup className="shoppings-list">
+            {items.map(({ id, name }) => (
+              <CSSTransition key={id} timeout={500} classNames="fade">
+                <ListGroupItem>
+                  <Button
+                    className="remove-btn"
+                    color="danger"
+                    size="sm"
+                    onClick={() => {
+                      this.setState(state => ({
+                        items: state.items.filter(item => item.id !== id)
+                      }));
+                    }}
+                  >
+                    &times;
+                  </Button>
+                  {name}
+                </ListGroupItem>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </ListGroup>
+      </Container>
+    );
+  }
+}
+
+export default VideosList;
