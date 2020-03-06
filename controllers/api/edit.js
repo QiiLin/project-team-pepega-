92f63dafc3737533fb4512bc31b1c9ebcf9ba1ff
Martin <liches@users.noreply.github.com>
2020-03-06 12:09:44 -0500

    edit merge working, just need id for videos and a way to find saved vids based on id

--- controllers/api/edit.js
+++ controllers/api/edit.js
@@ -3,57 +3,90 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 const ffmpeg = require('fluent-ffmpeg');
 const path = require('path');
+const fs = require('fs');
 
 // @route  POST /api/edit/merge/:id/:idSplice
 // @desc   Append video from idMerge to video from id
 // @access Private
 router.post("/merge/:id/:idMerge", auth, (req, res) => {
-  /*new ffmpeg(path.join(__dirname, "../../video_input/test2.mp4"))
-  .size("640x360")
-  .autopad()
-  .withVideoCodec('libx264')
-  .withAudioCodec('libmp3lame')
-  .withFpsInput(30)
-  .withVideoBitrate('650k')
-  .withAudioChannels(2)
-  .on('error', function(err) {
-    console.log('An error occurred1: ' + err.message);
-  })
-  .on('stderr', function(stderrLine) {
-    console.log('Stderr output: ' + stderrLine);
-  })
-  .save(path.join(__dirname, '../../video_output/tmp/test2_changed.mp4'));
+  //find filename of 2 vids from id & idMerge
+  //set to filepaths, path1 is id & path2 is idMerge
 
-  new ffmpeg(path.join(__dirname, "../../video_input/test1.mp4"))
-  .size("640x360")
-  .autopad()
-  .withVideoCodec('libx264')
-  .withAudioCodec('libmp3lame')
-  .withFpsInput(30)
-  .withVideoBitrate('650k')
-  .withAudioChannels(2)  
-  .on('error', function(err) {
-    console.log('An error occurred2: ' + err.message);
+  path1 = path.join(__dirname, "../../video_input/test1.mp4");
+  path2 = path.join(__dirname, "../../video_input/test2.mp4");
+  path1_base = path.basename(path1).replace(path.extname(path1),""); //filename w/o extension
+  path2_base = path.basename(path2).replace(path.extname(path2),"");
+  path1_tmp = path.join(path.dirname(path1), 'tmp', path1_base + "_mod.avi"); //temp file loc
+  path2_tmp = path.join(path.dirname(path2), 'tmp', path2_base + "_mod.avi");
+  pathOut_tmp = path.join(__dirname, '../../video_output/tmp');
+  pathOut_path = path.join(__dirname, "../../video_output/", path1_base + ".avi");
+  //__dirname, '../../video_input/tmp/test2_changed.avi'   "../../video_output/testmerged.avi"
+
+  ffmpeg.ffprobe(path1, function(err, metadata){
+    if(err) res.json('An error occurred [MergeResolution]: ' + err.message);
+
+    var width = metadata.streams[0].width;
+    var height = metadata.streams[0].height;
+
+    ffmpeg(path1)
+    .preset('divx')
+    .size(width + "x" + height)
+    .autopad()
+    .withFpsInput(30)
+    .on('progress', (progress) => {
+      console.log(`[Merge1]: ${JSON.stringify(progress)}`);
+    })
+    .on('error', function(err) {
+      res.json('An error occurred [Merge1]: ' + err.message);
+    })
+    /*.on('stderr', function(stderrLine) {
+      console.log('Stderr output [Merge1]: ' + stderrLine);
+    })*/
+    .on('end', function() {
+      ffmpeg(path2)
+      .preset('divx')  
+      .size("640x360")
+      .autopad()
+      .withFpsInput(30)
+      .on('progress', (progress) => {
+        console.log(`[Merge2]: ${JSON.stringify(progress)}`);
+      })
+      .on('error', function(err) {
+        fs.unlink(path1_tmp, (err) => {
+          if (err) console.log('Could not remove Merge1 tmp file:' + err);
+        });
+        res.json('An error occurred [Merge2]: ' + err.message);
+      })
+      /*.on('stderr', function(stderrLine) {
+        console.log('Stderr output [Merge2]:: ' + stderrLine);
+      })*/
+      .on('end', function() {
+        ffmpeg({ source: path1_tmp })
+        .mergeAdd(path2_tmp)
+        .on('progress', (progress) => {
+          console.log(`[MergeCombine]: ${JSON.stringify(progress)}`);
+        })
+        .on('error', function(err) {
+          res.json('An error occurred [MergeCombine]: ' + err.message);
+        })
+        /*.on('stderr', function(stderrLine) {
+          console.log('Stderr output [MergeCombine]:: ' + stderrLine);
+        })*/
+        .on('end', function() {
+          fs.unlink(path1_tmp, (err) => {
+            if (err) console.log('Could not remove Merge1 tmp file:' + err);
+          });
+          fs.unlink(path2_tmp, (err) => {
+            if (err) console.log('Could not remove Merge1 tmp file:' + err);
+          });
+          res.json('Merging finished !');
+        })
+        .mergeToFile(pathOut_path, pathOut_tmp);
+          })
+        .save(path2_tmp);
+    })
+    .save(path1_tmp);  
   })
-  .on('stderr', function(stderrLine) {
-    console.log('Stderr output: ' + stderrLine);
-  })
-  .save(path.join(__dirname, '../../video_output/tmp/test1_changed.mp4'));*/
-  
-  new ffmpeg({ source: path.join(__dirname, "../../video_output/tmp/test1_changed.mp4") })
-  .mergeAdd(path.join(__dirname, "../../video_output/tmp/test2_changed.mp4"))
-  .on('error', function(err) {
-      console.log('An error occurred: ' + err.message);
-  })
-  .on('stderr', function(stderrLine) {
-    console.log('Stderr output: ' + stderrLine);
-  })
-  .on('end', function() {
-      console.log('Merging finished !');
-  })
-  //.save(path.join(__dirname, "../../video_output/testmerged.mp4"));
-  .mergeToFile(path.join(__dirname, "../../video_output/testmerged.mp4"), path.join(__dirname, '../../video_output/tmp'));
-  res.json();
 });
 
 // @route  POST /api/edit/:id/:timestampOld/:timestampNew
