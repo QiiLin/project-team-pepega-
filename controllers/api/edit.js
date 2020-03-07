0c3350b07e2215fe393468632c1fd24ad0886c0e
QiiLin <qii.lin@mail.utoronto.ca>
2020-03-06 20:58:42 -0500

    update caption api

--- controllers/api/edit.js
+++ controllers/api/edit.js
@@ -4,43 +4,63 @@ const auth = require("../../middleware/auth");
 const fs = require('fs');
 const path = require('path');
 const ffmpeg  = require('../../controllers/ff_path');
+
+
 // @route  POST /api/caption
 // @desc   Create caption for the selected video
 // @access Private
 router.post("/:id/caption", (req, res) => {
-    // first build the content
+    if (!req.body.data) {
+        return res.status(400).end("Bad argument: Missing data");
+    }
     let result = "";
     req.body.data.forEach( function (curr, index) {
         let temp = "" + (index + 1) + "\n";
-        temp += curr.time + "\n";
+        temp += curr.start_time + ' --> ' + curr.end_time + "\n";
         temp += curr.text + "\n";
         result += temp;
     });
 
-    var sub_path = path.join(__dirname, "/../../temp/subtitle/", req.params.id + "_sub.srt").replace(/\\/g, "\\\\\\\\").replace(":","\\\\:");
-    
-    console.log(result);
-    fs.writeFile( __dirname + '/../../temp/subtitle/'+ req.params.id + '_sub.srt', result, function (err) {
+    // TODO Change path according the database
+    let sub_path = path.join(__dirname, "/../../temp/subtitle/",
+        req.params.id + "_sub.srt")
+        .replace(/\\/g, "\\\\\\\\")
+        .replace(":","\\\\:");
+
+    let srt_path = __dirname + "/../../temp/subtitle/" +
+        req.params.id + "_sub.srt";
+    let input_path =  path.join(__dirname +  "/../../temp/video/test.mp4");
+
+    let out_path = path.join(__dirname + "/../../temp/video/out.mp4");
+    console.log(sub_path, srt_path, input_path,out_path);
+    fs.writeFile(srt_path, result, function (err) {
         if (err) throw err;
         console.log('Saved!');
-        // invoke ff
-        // let ff = ffmpeg(__dirname + '/../../temp/video/test.mp4');
-        // ff.videoFilters(['subtitles='+ __dirname + '/../../temp/subtitle/' + req.params.id + '_sub.srt']);
-        // ff.save(__dirname + '/../../temp/video/out.mp4');
         ffmpeg()
-            .input(__dirname + '/../../temp/video/test.mp4')
+            .input(input_path)
             .outputOptions([
               `-vf subtitles=${sub_path}` 
             ])
-            .output(__dirname + '/../../temp/video/out.mp4')
-            .on('stderr', function(stderrLine) {
-              console.log('Stderr output [Subtitle]:: ' + stderrLine);
+            .output(out_path)
+            .on('progress', (progress) => {
+                console.log(`[Caption]: ${JSON.stringify(progress)}`);
             })
+            .on('error', function(err) {
+                fs.unlink(srt_path, (err) => {
+                    if (err) console.log('Could not remove srt file:' + err);
+                });
+                res.status(500).json('An error occurred [Caption]: ' + err.message);
+            })
+            // .on('stderr', function(stderrLine) {
+            //   console.log('Stderr output [Subtitle]:: ' + stderrLine);
+            // })
             .on("end",function () {
-                console.log('finished');
+                fs.unlink(srt_path, (err) => {
+                    if (err) console.log('Could not remove Merge1 tmp file:' + err);
+                });
+                // TODO return data with path to access the file in the database
+                return res.status(200).end("we did it");
             }).run();
-        console.log('proceesed!');
-        return res.status(200).end("we did it");
     });
 });
 
