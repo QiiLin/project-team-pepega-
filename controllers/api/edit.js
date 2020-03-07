1c11285acf03db66e831391d6c6b0effc997ac4a
Martin <liches@users.noreply.github.com>
2020-03-06 19:16:57 -0500

    Working trim w/ hardcoded video path

--- controllers/api/edit.js
+++ controllers/api/edit.js
@@ -31,7 +31,7 @@ router.post("/merge/:id/:idMerge", auth, (req, res) => {
     .preset('divx')    
     .withFpsInput(30)
     .outputOptions([
-      `-vf scale=${width}:${height},setsar=1`
+      `-vf scale=${width}:${height},setsar=1` //Sample Aspect Ratio = 1.0
     ])
     .on('progress', (progress) => {
       console.log(`[Merge1]: ${JSON.stringify(progress)}`);
@@ -72,7 +72,7 @@ router.post("/merge/:id/:idMerge", auth, (req, res) => {
             if (err) console.log('Could not remove Merge1 tmp file:' + err);
           });
           fs.unlink(path2_tmp, (err) => {
-            if (err) console.log('Could not remove Merge1 tmp file:' + err);
+            if (err) console.log('Could not remove Merge2 tmp file:' + err);
           });
           res.json('An error occurred [MergeCombine]: ' + err.message);
         })
@@ -84,7 +84,7 @@ router.post("/merge/:id/:idMerge", auth, (req, res) => {
             if (err) console.log('Could not remove Merge1 tmp file:' + err);
           });
           fs.unlink(path2_tmp, (err) => {
-            if (err) console.log('Could not remove Merge1 tmp file:' + err);
+            if (err) console.log('Could not remove Merge2 tmp file:' + err);
           });
           res.json('Merging finished !');
         })
@@ -96,20 +96,123 @@ router.post("/merge/:id/:idMerge", auth, (req, res) => {
   })
 });
 
-// @route  POST /api/edit/cut/:id/:timestampOld/:timestampNew
+// @route  POST /api/edit/cut/:id/
 // @desc   Cut video section at timestampOld of video from id and move to timestampNew
 // @access Private
-router.post("/cut/:id/:timestampOld/:timestampNew", auth, (req, res) => {
-
+router.post("/cut/:id", auth, (req, res) => {
+  if(!req.body.timestampOldStart ||
+     !req.body.timestampOldEnd ||
+     !req.body.timestampNewStart ||
+     !req.body.timestampNewEnd) return res.status(400).end("timestamp required");
+  
 });
 
-// @route  POST /api/edit/trim/:id/:timestampTrim
-// @desc   Remove video section at timestampTrim
+
+// @route  POST /api/edit/trim/:id/
+// @desc   Remove video section at timestampStart & timestampEnd from body
 // @access Private
-router.post("/trim/:id/:timestampTrim", auth, (req, res) => {
+router.post("/trim/:id/", auth, (req, res) => {
+  if(!req.body.timestampStart || !req.body.timestampEnd) return res.status(400).end("timestamp required");
+  //find filename of vid from id
+  //set to path1
+
   path1 = path.join(__dirname, "../../video_input/test1.mp4");
-  path1_base = path.basename(path1).replace(path.extname(path1),""); //filename w/o extension
-  pathOut_path = path.join(__dirname, "../../video_output/", path1_base + ".avi");
+  var path1_base = path.basename(path1).replace(path.extname(path1),""); //filename w/o extension
+  var path1_tmp = path.join(path.dirname(path1), 'tmp', path1_base + "_1" + path.extname(path1)); //temp file loc
+  var path2_tmp = path.join(path.dirname(path1), 'tmp', path1_base + "_2" + path.extname(path1)); 
+  var pathOut_path = path.join(__dirname, "../../video_output/", path.basename(path1));
+  var pathOut_tmp = path.join(__dirname, '../../video_output/tmp');
+
+  ffmpeg.ffprobe(path1, function(err, metadata){
+    var duration = metadata.streams[0].duration; //vid duration in timebase unit
+    console.log(metadata.streams[0]);
+
+    ffmpeg({ source: path1 })
+    .inputOptions([
+      `-ss 0`,
+      `-to ${req.body.timestampStart}`
+    ])
+    .on('progress', (progress) => {
+      console.log(`[Trim1]: ${JSON.stringify(progress)}`);
+    })
+    /*.on('stderr', function(stderrLine) {
+      console.log('Stderr output [Trim1]: ' + stderrLine);
+    })*/
+    .on('error', function(err) {
+      res.json('An error occurred [Trim1]: ' + err.message);
+    })
+    .on('end', function() {
+      ffmpeg({ source: path1 })
+      .inputOptions([
+        `-ss ${req.body.timestampEnd}`,
+        `-to ${duration}`
+      ])
+      .on('progress', (progress) => {
+        console.log(`[Trim1]: ${JSON.stringify(progress)}`);
+      })
+      /*.on('stderr', function(stderrLine) {
+        console.log('Stderr output [Trim1]: ' + stderrLine);
+      })*/
+      .on('error', function(err) {
+        fs.unlink(path1_tmp, (err) => {
+          if (err) console.log('Could not remove Trim2 tmp file:' + err);
+        });
+        res.json('An error occurred [Trim2]: ' + err.message);
+      })
+      .on('end', function() {
+        ffmpeg({ source: path1_tmp })
+        .mergeAdd(path2_tmp)
+        .on('progress', (progress) => {
+          console.log(`[MergeCombine]: ${JSON.stringify(progress)}`);
+        })
+        .on('error', function(err) {
+          fs.unlink(path1_tmp, (err) => {
+            if (err) console.log('Could not remove Trim1 tmp file:' + err);
+          });
+          fs.unlink(path2_tmp, (err) => {
+            if (err) console.log('Could not remove Trim2 tmp file:' + err);
+          });
+          res.json('An error occurred [TrimCombine]: ' + err.message);
+        })
+        /*.on('stderr', function(stderrLine) {
+          console.log('Stderr output [MergeCombine]:: ' + stderrLine);
+        })*/
+        .on('end', function() {
+          fs.unlink(path1_tmp, (err) => {
+            if (err) console.log('Could not remove Trim1 tmp file:' + err);
+          });
+          fs.unlink(path2_tmp, (err) => {
+            if (err) console.log('Could not remove Trim2 tmp file:' + err);
+          });
+          res.json('Merging finished !');
+        })
+        .mergeToFile(pathOut_path, pathOut_tmp);
+      })
+      .save(path2_tmp)
+    })
+    .save(path1_tmp);
+  });
+
+  /*ffmpeg({ source: path1 })
+    .complexFilter([      
+      `[0:v]trim=start=00:00:00.000:end=00:00:02.000[av]`,
+      `[0:a]atrim=start=00:00:00.000:end=00:00:02.000[aa]`,
+      `[0:v]trim=start=00:00:05.000:end=00:00:21.000,setpts=PTS-STARTPTS[bv]`, //,setpts=PTS-STARTPTS[bv]
+      `[0:a]atrim=start=00:00:05.000:end=00:00:21.000,asetpts=PTS-STARTPTS[ba]`, //,asetpts=PTS-STARTPTS[ba]
+      `[av][aa][bv][ba]concat=:v=0:a=0[outv][outa]`
+    ])
+    .outputOptions([
+      `-map [outv]`,
+      `-map [outa]`
+    ])
+    .on('stderr', function(stderrLine) {
+      console.log('Stderr output [MergeTrim]: ' + stderrLine);
+    })
+    .on('end', function() {
+      res.json('Trim finished !');
+    })
+    .save(pathOut_path);*/ 
+  
 });
 
 module.exports = router;
