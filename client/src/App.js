c5e79fff58138ff9967f6b49a5afbd0a805d7ac0
QiiLin <qii.lin@mail.utoronto.ca>
2020-03-13 15:24:33 -0400

    Video Caption

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import store from "./store";
 import AppNavbar from "./components/AppNavbar";
 import VideosList from "./components/VideosList";
 import ItemModal from "./components/ItemModal";
+import VideoCaptionInput from "./components/VideoCaptionInput";
 import { Container } from "reactstrap";
 import { loadUser } from "./actions/authActions";
 import "../node_modules/video-react/dist/video-react.css";
@@ -24,6 +25,7 @@ class App extends Component {
             <ItemModal />
             <VideosList />
           </Container>
+          <VideoCaptionInput> </VideoCaptionInput>
         </div>
       </Provider>
     );
