ad20d47a6d61735980ca6b0186253c0a32cef220
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-03-15 17:37:59 -0400

    Trying to download file

--- client/src/components/editComponent/VideoList.js
+++ client/src/components/editComponent/VideoList.js
@@ -25,6 +25,7 @@ import {
 } from "../../actions/itemActions";
 import { Player, Shortcut, ControlBar, BigPlayButton } from "video-react";
 import PropTypes from "prop-types";
+import Download from "@axetroy/react-download";
 const path = require("path");
 
 class VideoList extends Component {
