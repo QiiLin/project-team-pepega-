145eb3c80ebcda57a0956091260134a45043984d
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-03-15 16:45:42 -0400

    Trying to download file

--- client/src/components/editComponent/VideoList.js
+++ client/src/components/editComponent/VideoList.js
@@ -25,6 +25,7 @@ import {
 } from "../../actions/itemActions";
 import { Player, Shortcut, ControlBar, BigPlayButton } from "video-react";
 import PropTypes from "prop-types";
+import Download from "@axetroy/react-download";
 const path = require("path");
 
 class VideoList extends Component {
@@ -50,94 +51,121 @@ class VideoList extends Component {
     this.props.downloadFile(id);
   };
 
-    render() {
-        const { items } = this.props.item;
-        console.timeLog(items);
-        return (
-            <div>
-                <Nav tabs>
-                    <NavItem>
-                        <NavLink
-                            className={ this.state.selectTab === '1'  ? 'active': '' }
-                            onClick={() => { this.toggle('1'); }}
-                        >
-                            Video
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink
-                            className={ this.state.selectTab === '2'  ? 'active': '' }
-                            onClick={() => { this.toggle('2'); }}
-                        >
-                            Audio
-                        </NavLink>
-                    </NavItem>
-                </Nav>                
-                <TabContent activeTab={this.state.selectTab}>
-                    <TabPane tabId="1">
-                        <Row>
-                        {items.map(({ _id, filename}) => (
-                            <Col sm="6" key={_id}>
-                                <Card body >
-                                <Button
-                                    variant="contained"
-                                    color="secondary"
-                                    startIcon={<DeleteIcon/>}
-                                    onClick={() => {this.props.deleteItem(_id)}}
-                                >
-                                    Delete
-                                </Button>
-                                    <CardTitle>{filename}</CardTitle>
-                                    <Player 
-                                      key={filename}>
-                                        <source src={"api/items/" + filename}/>
-                                        <Shortcut clickable={false} dblclickable={false} disabled/>
-                                        <ControlBar disabled/>
-                                        <BigPlayButton  disabled/>
-                                    </Player>
-                                    <ButtonGroup vertical>
-                                        <Button 
-                                        variant="contained"
-                                        color="primary"
-                                        endIcon={<VideoLibraryIcon/>}
-                                        onClick={ () => {this.props.setSelectItemOne(filename)}}>
-                                            Load to player one
-                                        </Button>
-                                        <Button 
-                                        variant="contained"
-                                        color="primary"
-                                        endIcon={<VideoLibraryIcon/>}
-                                        onClick={ () => {this.props.setSelectItemTwo(filename)}}>
-                                            Load to player two
-                                        </Button>
-                                    </ButtonGroup>
-                                </Card>
-                            </Col>
-                        ))}
-                        </Row>
-                    </TabPane>
-                    <TabPane tabId="2">
-                        <Row>
-                            {/*<Col sm="6">*/}
-                                {/*<Card body>*/}
-                                    {/*<CardTitle>Special Title Treatment</CardTitle>*/}
-                                    {/*<CardText>With supporting text below as a natural lead-in to additional content.</CardText>*/}
-                                    {/*<Button>Go somewhere</Button>*/}
-                                {/*</Card>*/}
-                            {/*</Col>*/}
-                            {/*<Col sm="6">*/}
-                                {/*<Card body>*/}
-                                    {/*<CardTitle>Special Title Treatment</CardTitle>*/}
-                                    {/*<CardText>With supporting text below as a natural lead-in to additional content.</CardText>*/}
-                                    {/*<Button>Go somewhere</Button>*/}
-                                {/*</Card>*/}
-                            {/*</Col>*/}
-                        </Row>
-                    </TabPane>
-                </TabContent>
-            </div>
-        );
-    }
+  pause() {
+    this.player.pause();
+  }
+
+  render() {
+    const { items } = this.props.item;
+    console.log("items: ", items);
+    return (
+      <div>
+        <Nav tabs>
+          <NavItem>
+            <NavLink
+              className={this.state.selectTab === "1" ? "active" : ""}
+              onClick={() => {
+                this.toggle("1");
+              }}
+            >
+              Video
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={this.state.selectTab === "2" ? "active" : ""}
+              onClick={() => {
+                this.toggle("2");
+              }}
+            >
+              Audio
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.selectTab}>
+          <TabPane tabId="1">
+            <Row>
+              {items.map(({ _id, filename }) => (
+                <Col sm="6" key={_id}>
+                  <Card body>
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      startIcon={<DeleteIcon />}
+                      onClick={() => {
+                        this.props.deleteItem(_id);
+                      }}
+                    >
+                      Delete
+                    </Button>
+                    <CardTitle>{filename}</CardTitle>
+                    <Player key={filename}>
+                      <source src={"api/items/" + filename} />
+                      <Shortcut
+                        clickable={false}
+                        dblclickable={false}
+                        disabled
+                      />
+                      <ControlBar disabled />
+                      <BigPlayButton disabled />
+                    </Player>
+                    <ButtonGroup vertical>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        endIcon={<VideoLibraryIcon />}
+                        onClick={() => {
+                          this.props.setSelectItemOne(filename);
+                        }}
+                      >
+                        Load to player one
+                      </Button>
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        endIcon={<VideoLibraryIcon />}
+                        onClick={() => {
+                          this.props.setSelectItemTwo(filename);
+                        }}
+                      >
+                        Load to player two
+                      </Button>
+                      {/* <Download file={filename}>Save</Download> */}
+                      {/* <a href={filename}>Save</a> */}
+                      <Button
+                        color="primary"
+                        onClick={() => this.downloadFile(_id)}
+                      >
+                        Save
+                      </Button>
+                    </ButtonGroup>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </TabPane>
+          <TabPane tabId="2">
+            <Row>
+              {/*<Col sm="6">*/}
+              {/*<Card body>*/}
+              {/*<CardTitle>Special Title Treatment</CardTitle>*/}
+              {/*<CardText>With supporting text below as a natural lead-in to additional content.</CardText>*/}
+              {/*<Button>Go somewhere</Button>*/}
+              {/*</Card>*/}
+              {/*</Col>*/}
+              {/*<Col sm="6">*/}
+              {/*<Card body>*/}
+              {/*<CardTitle>Special Title Treatment</CardTitle>*/}
+              {/*<CardText>With supporting text below as a natural lead-in to additional content.</CardText>*/}
+              {/*<Button>Go somewhere</Button>*/}
+              {/*</Card>*/}
+              {/*</Col>*/}
+            </Row>
+          </TabPane>
+        </TabContent>
+      </div>
+    );
+  }
 }
 // Mapping a redux state to a component property
 const mapStateToProps = state => ({
