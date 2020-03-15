daf425c77beaee412b213378bd270e6906ac9ce2
Martin <liches@users.noreply.github.com>
2020-03-15 19:47:52 -0400

    Revert "In progress of downloading a file"
    This reverts commit 365cb706a59fd2b27216ed6a515e4680df21a845.

--- client/src/components/editComponent/TimeLineSector.js
+++ client/src/components/editComponent/TimeLineSector.js
@@ -1,53 +1,53 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
-import Slider from "@material-ui/core/Slider";
+import {makeStyles} from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
 
-const useStyles = {
-  root: {
-    width: 300
-  }
-};
+
+const useStyles = ({
+    root: {
+        width: 300,
+    },
+});
 
 function valuetext(value) {
-  return `${value} %`;
+    return `${value} %`;
 }
 
 class TimeLineSector extends React.Component {
-  //TODO get data for the represented video and set the range of this correctly
-  constructor(props) {
-    super(props);
-    // Don't call this.setState() here!
-    this.state = { newValue: [0, 100] };
-    this.handleChange = this.handleChange.bind(this);
-  }
+    //TODO get data for the represented video and set the range of this correctly
+    constructor(props) {
+        super(props);
+        // Don't call this.setState() here!
+        this.state = {newValue: [0, 100]};
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-  handleChange = (event, newValue) => {
-    console.log(newValue);
-    this.setState(state => ({
-      newValue: newValue
-    }));
-    this.props.callback(newValue);
-  };
+    handleChange = (event, newValue) => {
+        console.log(newValue);
+        this.setState(state => ({
+            newValue: newValue
+        }));
+        this.props.callback(newValue);
+    };
 
-  render() {
-    console.log("TimeLineSector this.props: ", this.props);
-    // const classes = useStyles();
-    return (
-      <div className={useStyles.root}>
-        <Typography id="range-slider" gutterBottom>
-          {this.props.title}
-        </Typography>
-        <Slider
-          value={this.state.newValue}
-          onChange={this.handleChange}
-          valueLabelDisplay="auto"
-          aria-labelledby="range-slider"
-          getAriaValueText={valuetext}
-        />
-      </div>
-    );
-  }
+    render() {
+        // const classes = useStyles();
+        return (
+            <div className={useStyles.root}>
+                <Typography id="range-slider" gutterBottom>
+                   {this.props.title}
+                </Typography>
+                <Slider
+                    value={this.state.newValue}
+                    onChange={this.handleChange}
+                    valueLabelDisplay="auto"
+                    aria-labelledby="range-slider"
+                    getAriaValueText={valuetext}
+                />
+            </div>
+        );
+    }
 }
 
 
