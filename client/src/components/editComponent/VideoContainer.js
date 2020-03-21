a02c2fc3bc085c80e7efc10c814a9ad55b270d78
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-03-21 17:34:19 -0400

    progress

--- client/src/components/editComponent/VideoContainer.js
+++ client/src/components/editComponent/VideoContainer.js
@@ -1,122 +1,123 @@
-import React, {Component} from "react";
-import { Player } from 'video-react';
-import {connect} from "react-redux";
-import {
-    Nav,
-    NavItem,
-    NavLink,
-    Row,
-    TabContent,
-    TabPane
-} from "reactstrap";
-import {set_duration} from "../../actions/editActions";
+import React, { Component } from "react";
+import { Player } from "video-react";
+import { connect } from "react-redux";
+import { Nav, NavItem, NavLink, Row, TabContent, TabPane } from "reactstrap";
+import { set_duration } from "../../actions/editActions";
 
 class VideoContainer extends Component {
-    constructor(props) {
-        super(props);
-        // Don't call this.setState() here!
-        this.state = {selectTab: '1'};
-        this.toggle = this.toggle.bind(this);
-        this.changeCurrentTime = this.changeCurrentTime.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    // Don't call this.setState() here!
+    this.state = { selectTab: "1" };
+    this.toggle = this.toggle.bind(this);
+    this.changeCurrentTime = this.changeCurrentTime.bind(this);
+  }
 
-    toggle = tab => {
-        if (tab !== this.state.selectTab) {
-            this.setState(state => ({
-                selectTab: tab
-            }));
-        }
+  toggle = tab => {
+    if (tab !== this.state.selectTab) {
+      this.setState(state => ({
+        selectTab: tab
+      }));
+    }
+  };
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    // subscribe state change
+    this.player.subscribeToStateChange(this.handleStateChange.bind(this));
+  }
+
+  changeCurrentTime(seconds) {
+    return () => {
+      this.player.seek(seconds);
     };
+  }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        // subscribe state change
-        this.player.subscribeToStateChange(this.handleStateChange.bind(this));
-    }
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.item.selectItemOne !== this.props.item.selectItemOne;
+  }
 
-
-    changeCurrentTime(seconds) {
-        return () => {
-            this.player.seek(seconds);
-        };
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        return  nextProps.item.selectItemOne !==this.props.item.selectItemOne;
-    }
-
-    handleStateChange(state) {
-        // copy player state to this component's state
-        this.setState({
-            player: state
-        });
-        console.log("weqweqweqwe");
-        const { player } = this.player.getState();
-        this.props.set_duration(player.duration);
-    }
-    render() {
-        // Note selectedFile is from VideoList
-        // TODO: Update the placeholder for video
-        const {selectItemOne, selectItemTwo} = this.props.item;
-        return (
-            <div>
-                <Nav tabs>
-                    <NavItem>
-                        <NavLink
-                            className={this.state.selectTab === '1' ? 'active' : ''}
-                            onClick={() => {
-                                this.toggle('1');
-                            }}
-                        >
-                            Video One
-                        </NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink
-                            className={this.state.selectTab === '2' ? 'active' : ''}
-                            onClick={() => {
-                                this.toggle('2');
-                            }}
-                        >
-                            Video Two
-                        </NavLink>
-                    </NavItem>
-                </Nav>
-                <TabContent activeTab={this.state.selectTab}>
-                    <TabPane tabId="1">
-                        <Row>
-                            {selectItemOne ?
-                                (<Player key={selectItemOne}
-                                         ref={ player => {
-                                             this.player = player;
-                                         }}>
-                                    <source src={"api/items/" + selectItemOne}/>
-                                </Player>) : (
-                                    <Player key={selectItemOne}
-                                            ref={player => {
-                                                this.player = player;
-                                            }}>
-                                        <source src={"http://www.w3schools.com/html/mov_bbb.mp4"}/>
-                                    </Player>
-                                )}
-                        </Row>
-                    </TabPane>
-                    <TabPane tabId="2">
-                        <Row>
-                            {selectItemTwo ? (<Player key={selectItemOne}>
-                                <source src={"api/items/" + selectItemTwo}/>
-                            </Player>) : (<p> No video </p>)}
-                        </Row>
-                    </TabPane>
-                </TabContent>
-            </div>
-        );
-    }
+  handleStateChange(state) {
+    // copy player state to this component's state
+    this.setState({
+      player: state
+    });
+    console.log("weqweqweqwe");
+    const { player } = this.player.getState();
+    this.props.set_duration(player.duration);
+  }
+  render() {
+    // Note selectedFile is from VideoList
+    // TODO: Update the placeholder for video
+    const { selectItemOne, selectItemTwo } = this.props.item;
+    return (
+      <div>
+        <Nav tabs>
+          <NavItem>
+            <NavLink
+              className={this.state.selectTab === "1" ? "active" : ""}
+              onClick={() => {
+                this.toggle("1");
+              }}
+            >
+              Video One
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={this.state.selectTab === "2" ? "active" : ""}
+              onClick={() => {
+                this.toggle("2");
+              }}
+            >
+              Video Two
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.selectTab}>
+          <TabPane tabId="1">
+            <Row>
+              {selectItemOne ? (
+                <Player
+                  key={selectItemOne}
+                  ref={player => {
+                    this.player = player;
+                  }}
+                >
+                  <source src={"api/items/" + selectItemOne} />
+                </Player>
+              ) : (
+                <Player
+                  key={selectItemOne}
+                  ref={player => {
+                    this.player = player;
+                  }}
+                >
+                  <source src={"http://www.w3schools.com/html/mov_bbb.mp4"} />
+                </Player>
+              )}
+            </Row>
+          </TabPane>
+          <TabPane tabId="2">
+            <Row>
+              {selectItemTwo ? (
+                <Player key={selectItemOne}>
+                  <source src={"api/items/" + selectItemTwo} />
+                </Player>
+              ) : (
+                <p> No video </p>
+              )}
+            </Row>
+          </TabPane>
+        </TabContent>
+      </div>
+    );
+  }
 }
 
 // Mapping a redux state to a component property
 const mapStateToProps = state => ({
-    // item because we called it that in reducers/index.js (root reducer)
-    item: state.item,
-    isAuthenticated: state.auth.isAuthenticated
+  // item because we called it that in reducers/index.js (root reducer)
+  item: state.item,
+  isAuthenticated: state.auth.isAuthenticated
 });
-export default connect(mapStateToProps, {set_duration})(VideoContainer);
+export default connect(mapStateToProps, { set_duration })(VideoContainer);
