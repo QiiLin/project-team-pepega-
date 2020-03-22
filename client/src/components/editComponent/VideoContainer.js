07265087558bf4fa1e67d89eb0c6d1e7b37a3383
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-03-21 22:36:30 -0400

    Removed some video-react imports that will not be used anymore

--- client/src/components/editComponent/VideoContainer.js
+++ client/src/components/editComponent/VideoContainer.js
@@ -1,19 +1,7 @@
 import React, { Component } from "react";
-import {
-    AppBar,
-    Tabs,
-    Tab,
-    Box
-} from '@material-ui/core/';
-import {setDurationOne, setDurationTwo} from "../../actions/editActions";
-import {
-  Player,
-  BigPlayButton,
-  LoadingSpinner,
-  ControlBar,
-  ReplayControl,
-  ForwardControl
-} from "video-react";
+import { AppBar, Tabs, Tab, Box } from "@material-ui/core/";
+import { setDurationOne, setDurationTwo } from "../../actions/editActions";
+import { Player } from "video-react";
 import { connect } from "react-redux";
 
 class VideoContainer extends Component {
@@ -24,89 +12,107 @@ class VideoContainer extends Component {
     this.changeCurrentTime = this.changeCurrentTime.bind(this);
   }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        // subscribe state change
-        this.player1.subscribeToStateChange(this.handleStateChangeOne.bind(this));
-        this.player2.subscribeToStateChange(this.handleStateChangeTwo.bind(this));
-    }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    // subscribe state change
+    this.player1.subscribeToStateChange(this.handleStateChangeOne.bind(this));
+    this.player2.subscribeToStateChange(this.handleStateChangeTwo.bind(this));
+  }
 
-    changeCurrentTime(seconds) {
-        return () => {
-            this.player1.seek(seconds);
-        };
-    }
+  changeCurrentTime(seconds) {
+    return () => {
+      this.player1.seek(seconds);
+    };
+  }
 
-    handleStateChangeOne(state) {
-        // copy player state to this component's state
-        this.setState({
-            player1: state
-        });
-        console.log("weqweqweqwe");
-        const { player } = this.player1.getState();
-        this.props.setDurationOne(player.duration);
-    }
+  handleStateChangeOne(state) {
+    // copy player state to this component's state
+    this.setState({
+      player1: state
+    });
+    console.log("weqweqweqwe");
+    const { player } = this.player1.getState();
+    this.props.setDurationOne(player.duration);
+  }
 
-    handleStateChangeTwo(state) {
-        // copy player state to this component's state
-        this.setState({
-            player2: state
-        });
-        console.log("weqweqweqwe2");
-        const { player } = this.player2.getState();
-        this.props.setDurationTwo(player.duration);
-    }
+  handleStateChangeTwo(state) {
+    // copy player state to this component's state
+    this.setState({
+      player2: state
+    });
+    console.log("weqweqweqwe2");
+    const { player } = this.player2.getState();
+    this.props.setDurationTwo(player.duration);
+  }
 
-    handleTabClick = (event, value) => {
-        this.setState({selectTab: value});
-    }
-    
-    render() {
-        // Note selectedFile is from VideoList
-        // TODO: Update the placeholder for video
-        const {selectItemOne, selectItemTwo} = this.props.item;
-        return (
-            <div>
-                <AppBar position="static">
-                    <Tabs value={this.state.selectTab} onChange={this.handleTabClick}>
-                        <Tab label="Player One" value='1' />
-                        <Tab label="Player Two" value='2'/>
-                    </Tabs>
-                </AppBar>
-                <Box display={this.state.selectTab === '1' ? "block" : "none"} value='1'>
-                    {selectItemOne ?
-                        (<Player key={selectItemOne}
-                                    ref={player => {
-                                        this.player1 = player;
-                                    }}>
-                            <source src={"api/items/" + selectItemOne}/>
-                        </Player>) : (
-                            <Player key={selectItemOne}
-                                    ref={player => {
-                                        this.player1 = player;
-                                    }}>
-                                <source src={"http://www.w3schools.com/html/mov_bbb.mp4"}/>
-                            </Player>
-                        )}
-                </Box>
-                <Box display={this.state.selectTab === '2' ? "block" : "none"} value='2'>
-                    {selectItemTwo ?
-                        (<Player key={selectItemTwo}
-                                    ref={ player => {
-                                        this.player2 = player;
-                                    }}>
-                            <source src={"api/items/" + selectItemTwo}/>
-                        </Player>) : (
-                            <Player key={selectItemTwo}
-                                    ref={player => {
-                                        this.player2 = player;
-                                    }}>
-                                <source src={"http://techslides.com/demos/sample-videos/small.webm"}/>
-                            </Player>
-                        )}
-                </Box>
-            </div>
-        );
-    }
+  handleTabClick = (event, value) => {
+    this.setState({ selectTab: value });
+  };
+
+  render() {
+    // Note selectedFile is from VideoList
+    // TODO: Update the placeholder for video
+    const { selectItemOne, selectItemTwo } = this.props.item;
+    return (
+      <div>
+        <AppBar position="static">
+          <Tabs value={this.state.selectTab} onChange={this.handleTabClick}>
+            <Tab label="Player One" value="1" />
+            <Tab label="Player Two" value="2" />
+          </Tabs>
+        </AppBar>
+        <Box
+          display={this.state.selectTab === "1" ? "block" : "none"}
+          value="1"
+        >
+          {selectItemOne ? (
+            <Player
+              key={selectItemOne}
+              ref={player => {
+                this.player1 = player;
+              }}
+            >
+              <source src={"api/items/" + selectItemOne} />
+            </Player>
+          ) : (
+            <Player
+              key={selectItemOne}
+              ref={player => {
+                this.player1 = player;
+              }}
+            >
+              <source src={"http://www.w3schools.com/html/mov_bbb.mp4"} />
+            </Player>
+          )}
+        </Box>
+        <Box
+          display={this.state.selectTab === "2" ? "block" : "none"}
+          value="2"
+        >
+          {selectItemTwo ? (
+            <Player
+              key={selectItemTwo}
+              ref={player => {
+                this.player2 = player;
+              }}
+            >
+              <source src={"api/items/" + selectItemTwo} />
+            </Player>
+          ) : (
+            <Player
+              key={selectItemTwo}
+              ref={player => {
+                this.player2 = player;
+              }}
+            >
+              <source
+                src={"http://techslides.com/demos/sample-videos/small.webm"}
+              />
+            </Player>
+          )}
+        </Box>
+      </div>
+    );
+  }
 }
 
 // Mapping a redux state to a component property
@@ -115,4 +121,6 @@ const mapStateToProps = state => ({
   item: state.item,
   isAuthenticated: state.auth.isAuthenticated
 });
-export default connect(mapStateToProps, {setDurationOne, setDurationTwo})(VideoContainer);
+export default connect(mapStateToProps, { setDurationOne, setDurationTwo })(
+  VideoContainer
+);
