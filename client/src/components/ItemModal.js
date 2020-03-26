1d3f98a8617b1f29a03bb8b949218699bf24304b
HarrisonFok <pak.fok@mail.utoronto.ca>
2020-03-26 18:32:09 -0400

    Revert "Installing a library messed up everything"
    This reverts commit d632a7de5e1be5252a5589a97fa09f595f10d2fc.

--- client/src/components/ItemModal.js
+++ client/src/components/ItemModal.js
@@ -65,13 +65,10 @@ class ItemModal extends Component {
         {this.props.isAuthenticated ? (
           <div>
             <h3>Please upload a video</h3>
-            <h4>You can:</h4>
-            <ui>
-              <li>merge two videos</li>
-              <li>add transition effects</li>
-              <li>trim videos or</li>
-              <li>add captions to videos</li>
-            </ui>
+            <h4>
+              You can merge two videos, add transition effects, trim videos, or
+              add captions to videos
+            </h4>
             <Button
               color="dark"
               style={{ marginBottom: "50px" }}
