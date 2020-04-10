bf7c912a734f1f205c11ed3c0dc861390947fb7d
HarrisonFok <harrison.fok@gmail.com>
2020-04-10 16:22:20 -0400

    investigating security

--- controllers/api/edit.js
+++ controllers/api/edit.js
@@ -74,7 +74,7 @@ function generateThumbnail(id, filename) {
 // @route  POST /api/caption
 // @desc   Create caption for the selected video
 // @access Private
-router.post("/caption/:id", (req, res) => {
+router.post("/caption/:id", auth, (req, res) => {
   res.set("Content-Type", "text/plain");
   console.log(req.body.data);
   if (!req.body.data) {
@@ -171,7 +171,7 @@ router.post("/caption/:id", (req, res) => {
 // @route  POST /api/edit/merge/
 // @desc   Append video from idMerge to video from id
 // @access Private
-router.post("/merge", upload.none(), (req, res) => {
+router.post("/merge", upload.none(), auth, (req, res) => {
   res.set("Content-Type", "text/plain");
   console.log(req.body.curr_vid_id)
   console.log(req.body.merge_vid_id)
@@ -301,7 +301,7 @@ router.post("/merge", upload.none(), (req, res) => {
 // @route  POST /api/edit/cut/:id/
 // @desc   Cut video section at timestampOld of video from id and move to timestampNew
 // @access Private
-router.post("/cut/:id", (req, res) => {
+router.post("/cut/:id", auth, (req, res) => {
   res.set("Content-Type", "text/plain");
   if (!req.body.timestampOldStart || !req.body.timestampDuration)
     return res.status(400).end("timestamp required");
@@ -345,7 +345,7 @@ router.post("/cut/:id", (req, res) => {
 // @route  POST /api/edit/trim/:id/
 // @desc   Remove video section at timestampStart & timestampEnd from body
 // @access Private
-router.post("/trim/:id/", upload.none(), (req, res) => {
+router.post("/trim/:id/", upload.none(), auth, (req, res) => {
   let timestampStart = req.body.timestampStart;
   let timestampEnd = req.body.timestampEnd;
   console.log("start: ", timestampStart);
@@ -499,7 +499,7 @@ router.post("/trim/:id/", upload.none(), (req, res) => {
 // @route  POST /api/edit/transition/:id/
 // @desc   Add transition effects in a video at a timestamp
 // @access Private
-router.post("/transition/:id", upload.none(), (req, res) => {
+router.post("/transition/:id", upload.none(), auth, (req, res) => {
   // console.log(req.body.transition_paddingVidWidth);
   // console.log(req.body.transition_paddingVidHeight);
   // console.log(req.body.transition_paddingColor);
@@ -555,7 +555,7 @@ router.post("/transition/:id", upload.none(), (req, res) => {
 
 // @route POST /api/edit/saveMP3
 // @desc  Save the user recording into the database once the Stop button is pressed
-router.post("/saveMP3", upload.single("mp3file"), async (req, res) => {
+router.post("/saveMP3", upload.single("mp3file"), auth, async (req, res) => {
   // console.log("edit.js saveMP3");
   // console.log("file in backend: ", req.file)
   let gfs = await gfs_prim;
