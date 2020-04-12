d24ae5367ced8f59ac230f315f22a0c6d16d271b
Martin <liches@users.noreply.github.com>
2020-04-12 16:03:02 -0400

    Fixed validateDOMNesting warnings

--- client/src/components/popUpComponent/OperationPop.js
+++ client/src/components/popUpComponent/OperationPop.js
@@ -53,9 +53,7 @@ class OperationPop extends React.Component {
           aria-labelledby="max-width-dialog-title">
           <DialogTitle id="max-width-dialog-title">Operation Dialog</DialogTitle>
           <DialogContent>
-            <DialogContentText>
               {this.renderProgressContent()}
-            </DialogContentText>
           </DialogContent>
           <DialogActions>
             <Button onClick={this.handleClose} color="primary">
